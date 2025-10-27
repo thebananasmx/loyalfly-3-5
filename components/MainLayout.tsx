322813cf70baf2182982d2793bb3a4727449aacd
thebananasmx <hector@thebananas.com.mx>
2025-10-26 20:22:34 -0600

    feat: Add promotional info bar on homepage
    Introduces a new `InfoBar` component displayed exclusively on the homepage. This bar contains a promotional message and a call to action for new user registration. It is dismissible by the user.
    
    Updates `package.json` and `metadata.json` to reflect the new version number.

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,12 +1,17 @@
 
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import InfoBar from './InfoBar';
 
 const MainLayout: React.FC = () => {
+  const location = useLocation();
+  const showInfoBar = location.pathname === '/';
+
   return (
     <div className="min-h-screen flex flex-col font-sans text-base text-gray-800">
+      {showInfoBar && <InfoBar />}
       <Header />
       <main className="flex-grow">
         <Outlet />
