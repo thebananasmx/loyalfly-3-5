3abb46982fa85641da2d0cf3587eb1201a2459b6
thebananasmx <hector@thebananas.com.mx>
2026-04-05 12:35:43 -0600

    feat: Update landing page hero image
    Enhances the visual appeal of the landing page by replacing the hero image with a higher resolution version. This improves the overall user experience and presentation of the Loyalfly service.

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -180,7 +180,7 @@ const LandingPage: React.FC = () => {
                         {/* Right side: Image */}
                         <div className="md:w-1/2">
                             <img
-                                src="https://res.cloudinary.com/dg4wbuppq/image/upload/f_auto,q_auto,w_1000/v1762014799/loyalfly_hero_p4fkee.png"
+                                src="https://res.cloudinary.com/dg4wbuppq/image/upload/f_auto,q_auto,w_1200/v1775413956/loyalfly_hero_p4fkee.png"
                                 alt="Tarjeta de lealtad digital de Loyalfly en un smartphone"
                                 className="rounded-lg shadow-xl"
                             />
